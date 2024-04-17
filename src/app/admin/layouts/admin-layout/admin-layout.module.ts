845b1ef102cb481d5c9e855a38b15b0fd5bc6af5
Nguyen Tran Hoang <hoangnguyen@interspace.vn>
2024-04-17 14:41:11 +0700

    init pj

---
+++ src/app/admin/layouts/admin-layout/admin-layout.module.ts
@@ -0,0 +1,52 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AdminLayoutRoutes } from './admin-layout.routing';
+import { DashboardComponent } from '../../module/dashboard/dashboard.component';
+import { UserProfileComponent } from '../../module/user-profile/user-profile.component';
+import { TableListComponent } from '../../module/table-list/table-list.component';
+import { TypographyComponent } from '../../module/typography/typography.component';
+import { IconsComponent } from '../../module/icons/icons.component';
+import { MapsComponent } from '../../module/maps/maps.component';
+import { NotificationsComponent } from '../../module/notifications/notifications.component';
+import { UpgradeComponent } from '../../module/upgrade/upgrade.component';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
+import { MatRippleModule } from '@angular/material/core';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatSelectModule } from '@angular/material/select';
+import { UsersComponent } from 'app/admin/module/users/users.component';
+import { CategoriesComponent } from 'app/admin/module/categories/categories.component';
+import { ProductsComponent } from 'app/admin/module/products/products.component';
+
+@NgModule({
+    imports: [
+        CommonModule,
+        RouterModule.forChild(AdminLayoutRoutes),
+        FormsModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatRippleModule,
+        MatFormFieldModule,
+        MatInputModule,
+        MatSelectModule,
+        MatTooltipModule,
+    ],
+    declarations: [
+        DashboardComponent,
+        UserProfileComponent,
+        TableListComponent,
+        TypographyComponent,
+        IconsComponent,
+        MapsComponent,
+        NotificationsComponent,
+        UpgradeComponent,
+        UsersComponent,
+        CategoriesComponent,
+        ProductsComponent
+    ]
+})
+
+export class AdminLayoutModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,29 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app.routing';
 import { AppComponent } from './app.component';
+import { AdminLayoutComponent } from './admin/layouts/admin-layout/admin-layout.component';
+import { ComponentsModule } from './admin/components/components.module';
+import { AdminModule } from './admin/admin.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    imports: [
+        BrowserAnimationsModule,
+        FormsModule,
+        ReactiveFormsModule,
+        HttpClientModule,
+        ComponentsModule,
+        RouterModule,
+        AppRoutingModule,
+    ],
+    declarations: [
+        AppComponent,
+        AdminLayoutComponent
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
